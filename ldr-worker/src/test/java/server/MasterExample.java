e0c2084bddf911ea8d8276ad18360069b6b3b120
Lev Saskov <lev.saskov@vk.team>
2023-11-12 15:14:09 +0300

    With some tests

---
+++ ldr-worker/src/test/java/server/MasterExample.java
@@ -0,0 +1,13 @@
+package server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class MasterExample {
+    private void make(List<String> workersUrls) {
+    }
+}
+
